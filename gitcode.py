b83f034ee6fdff8ce67041c987c227697120111b
Synderis <51927796+Synderis@users.noreply.github.com>
2022-07-23 14:49:07 -0400

    Update gitcode.py

--- gitcode.py
+++ gitcode.py
@@ -1,45 +1,51 @@
-#This is a test 
+# This is a test
 
 import random
 import numpy as np
-def reselect():
 
+
+def reselect():
     x = 0
     base = 136.3 * 0.5774
-    mge = "mage"
-    rge = "range"
+    mge = 1
+    rge = 2
     t = 0
     u = 0
     c = 0
     z = 0
-    r_prob_hit_new = 100 - pow(base, c)
-    l_prob_hit_new = pow(base, c)
-    choice_initial = ['mge', 'rge']
+    r_prob_hit_new = 1 - (pow(base, z) / 100)
+    l_prob_hit_new = (pow(base, z) / 100)
+    choice_initial = np.array([1, 2])
+    print('test')
     while c == 0:
-        initial_result = np.random.choice(choice_initial, 2, p=[0.50, 0.50])
+        initial_result = np.random.choice(choice_initial, p=[0.50, 0.50])
         print(initial_result)
-        if initial_result == mge:
+        if initial_result.any() < 2:
             t += 1
             c += 1
             z += 1
             print('magetest')
-        elif initial_result == rge:
+        elif initial_result > 1:
             u += 1
             c += 1
             z += 1
             print('rangetest')
-        else:
-            continue
 
         for z in range(1, 30):
             if u > 0:
-                post_result = np.random.choice(choice_initial, 2, p=[l_prob_hit_new, r_prob_hit_new])
+                post_result = np.random.choice(choice_initial, p=[l_prob_hit_new, r_prob_hit_new])
                 print(post_result)
+                print('success')
                 u += 1
                 z = u
-
+                x += 1
             elif t > 0:
-                post_result = np.random.choice(choice_initial, 2, p=[l_prob_hit_new, r_prob_hit_new])
+                post_result = np.random.choice(choice_initial, p=[r_prob_hit_new, l_prob_hit_new])
                 print(post_result)
+                print('success2')
                 t += 1
                 z = t
+                x += 1
+
+
+reselect()
