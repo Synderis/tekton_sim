95bc23393a185c37fff1c3302960519a4b81470f
Synderis <51927796+Synderis@users.noreply.github.com>
2022-07-23 14:55:43 -0400

    Update gitcode.py

--- gitcode.py
+++ gitcode.py
@@ -1,51 +1,44 @@
-# This is a test
-
 import random
 import numpy as np
-
-
+##hgj
 def reselect():
+
     x = 0
     base = 136.3 * 0.5774
-    mge = 1
-    rge = 2
-    t = 0
-    u = 0
-    c = 0
-    z = 0
-    r_prob_hit_new = 1 - (pow(base, z) / 100)
-    l_prob_hit_new = (pow(base, z) / 100)
-    choice_initial = np.array([1, 2])
-    print('test')
-    while c == 0:
-        initial_result = np.random.choice(choice_initial, p=[0.50, 0.50])
+    mge = "mage"
+    rge = "range"
+    mage_count = 0
+    range_count = 0
+    initial_qualifier = 0
+    auto_count = 0
+    r_prob_hit_new = 100 - pow(base, auto_count)
+    l_prob_hit_new = pow(base, auto_count)
+    choice_initial = ['mge', 'rge']
+    while initial_qualifier == 0:
+        initial_result = np.random.choice(choice_initial, 2, p=[0.50, 0.50])
         print(initial_result)
-        if initial_result.any() < 2:
-            t += 1
-            c += 1
-            z += 1
+        if initial_result == mge:
+            mage_count += 1
+            initial_qualifier += 1
+            auto_count += 1
             print('magetest')
-        elif initial_result > 1:
-            u += 1
-            c += 1
-            z += 1
+        elif initial_result == rge:
+            range_count += 1
+            initial_qualifier += 1
+            auto_count += 1
             print('rangetest')
-##think it might need another function to call here 
+        else:
+            continue
+##think there probably needs to be another function here
         for z in range(1, 30):
-            if u > 0:
-                post_result = np.random.choice(choice_initial, p=[l_prob_hit_new, r_prob_hit_new])
+            if range_count > 0:
+                post_result = np.random.choice(choice_initial, 2, p=[l_prob_hit_new, r_prob_hit_new])
                 print(post_result)
-                print('success')
-                u += 1
-                z = u
-                x += 1
-            elif t > 0:
-                post_result = np.random.choice(choice_initial, p=[r_prob_hit_new, l_prob_hit_new])
+                range_count += 1
+                z = range_count
+
+            elif mage_count > 0:
+                post_result = np.random.choice(choice_initial, 2, p=[l_prob_hit_new, r_prob_hit_new])
                 print(post_result)
-                print('success2')
-                t += 1
-                z = t
-                x += 1
-
-
-reselect()
+                mage_count += 1
+                z = mage_count
